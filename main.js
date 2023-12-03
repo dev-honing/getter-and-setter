c20d56f9e85f051a89ea1d4e4994c219bd345f7b
Ho Nyeong <dev.honing@gmail.com>
2023-12-03 17:47:50 +0900

    getter로 isAdult() 메서드를 Dog 클래스에 추가함.
    - 개의 성인 여부를 확인하는 기능으로 추가

--- main.js
+++ main.js
@@ -24,6 +24,14 @@ class Dog {
   bark() {
     console.log('멍멍!');
   }
+  /**
+   * 개의 성인 여부를 확인하는 getter
+   * (3살이면 성인으로 판단)
+   * @returns {boolean} 개가 성인이면 true, 그렇지 않으면 false
+   */
+  get isAdult() {
+    return this.age >=3;
+  }
 }
 
 // Dog 생성자 함수를 호출하여 새로운 객체를 생성하고 초기화
@@ -33,3 +41,6 @@ console.log(myDog); // Dog { name: '멍멍이', age: 3 }
 
 // 개가 짖는 소리 출력
 myDog.bark(); // 멍멍!
+
+// 개의 성인 여부를 확인
+console.log(myDog.isAdult); // true
