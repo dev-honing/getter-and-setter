43852d31439c444fbef2660464168e4e8f588f81
Ho Nyeong <dev.honing@gmail.com>
2023-12-03 20:20:42 +0900

    bark() 메서드를 추가하고 개가 짖는 소리를 출력

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ class Dog:
   def __init__(self, name, age):
     self.name = name
     self.age = age
+  
+  def bark(self):
+    print('멍멍!')
 
 # Dog 클래스의 my_dog 인스턴스를 생성
 my_dog = Dog('멍멍이', 3)
 
-# 인스턴스를 통해 속성에 접근해서 출력
+# my_dog 인스턴스의 속성에 접근해서 출력
 print(my_dog.name) # 멍멍이
+
+# 개가 짖는 소리 출력
+my_dog.bark() # 멍멍!
