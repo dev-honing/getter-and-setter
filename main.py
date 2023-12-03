d7664e17157f806c294aa0e8a47913500217bb6b
Ho Nyeong <dev.honing@gmail.com>
2023-12-03 20:46:39 +0900

    주석 설명을 정돈하고 세 개의 따옴표를 사용해 함수와 메서드에 대한 자세한 주석 작성

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 # main.py
 
 class Dog:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
-  
-  def bark(self):
-    print('멍멍!')
+    def __init__(self, name, age):
+        """
+        Dog 클래스의 생성자 함수.
 
-# Dog 클래스의 my_dog 인스턴스를 생성
+        :param name: 개의 이름.
+        :type name: str
+        :param age: 개의 나이.
+        :type age: int
+        """
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        """
+        개가 짖는 소리를 출력하는 메서드.
+        """
+        print('멍멍!')
+
+# Dog 클래스를 호출하여 새로운 객체(my_dog)를 생성하고 초기화
 my_dog = Dog('멍멍이', 3)
 
-# my_dog 인스턴스의 속성에 접근해서 출력
-print(my_dog.name) # 멍멍이
+# 새로운 객체 my_dog를 출력
+print(my_dog.name)  # 멍멍이
 
 # 개가 짖는 소리 출력
-my_dog.bark() # 멍멍!
+my_dog.bark()  # 멍멍!
